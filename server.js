000df6806ec06bd512d641a2aaffb38e86408d20
yussaaa <yusa.li@mail.utoronto.ca>
2023-06-01 15:17:47 -0400

    commit all

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const app = express();
 
@@ -25,6 +27,7 @@ const database = {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json(database.users);
