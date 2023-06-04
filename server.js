04f4f3d2ced3329b90e9931a16dfc32ec321874a
yussaaa <yusa.li@mail.utoronto.ca>
2023-06-04 16:20:29 -0400

    Using exact same db config format

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   },
 });
